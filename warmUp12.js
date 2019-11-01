f9e5b29f7c9f3b67fe652a869cc1f84a2a77399d
waj949 <wajdihamrouni1993@gmail.com>
2019-11-01 09:50:20 +0100

    warmup12 1stversion

--- warmUp12.js
+++ warmUp12.js
@@ -22,3 +22,35 @@ Technical Details
     -The maximum size of a range will be 100 integers
     -The starting number of a range will be: 0 < n < 100
  */
+
+ function mysteryRange(inputString,range){
+    // split the input string into  array of numbers
+    var arr = inputString.split('');
+    var array = [];
+    // this empty array will hold the first group of Integers < 10 .. it's length is range
+    for(let i = 0; i < range; i++){
+        array.push(arr[i]);
+    }
+    console.log(array)
+    // arr1 will hold the rest of arr : 
+    var arr1 = arr.slice(range);
+    // console.log(array)
+    // console.log(arr)
+    // console.log(arr1)
+
+    for(let i = 0; i < range; i++){
+        //looping threw array fixing one item
+        var n1 = Number(array[i])
+        for(let j = 0; j < arr1.length; j++){
+            // looping threw arr1 checking of the range condition and updating array 
+            var n2 = Number(arr1[j]);
+            if(n2 +n2 < 100 && n1 + n2 > 0){
+                array[i] = n1 + n2
+            }
+        }
+    }
+    //console.log(array);
+    var max = Math.max(...array); // maximum
+    var min = Math.min(...array); // minimum
+    return '['+ min + ',' + max + ']'
+ }
